c957a687c8a0234f47727192aa7e7a571bc792b1
Lokeswar Gahir <lokeswargahir349@gmail.com>
2023-07-07 23:16:58 +0530

    read button function is partially complere

--- frontend/hello.js
+++ frontend/hello.js
@@ -1,53 +1,30 @@
+let token
 class User{
-    constructor(token){
-        this.token=token
+    constructor(){
     }
-    create(){
-        console.log("creating...")
-    }
-    read(){
-        console.log("reading...")
-    }
-    update(){
-        console.log("updating...")
-    }
-    delete(){
-        console.log("deleting...")
-    }
-}
-
-
-
-function reset() {
-    document.getElementById("login-window").innerHTML = "";
-    document.getElementById("res-div").innerHTML = "";
-    document.getElementById("tablehead").innerHTML = "";
-    document.getElementById("userData").innerHTML = "";
-    document.getElementById("sendingtokenwindow").innerHTML = "";
-}
-
-function getToken() {
-    const form = document.getElementById("form")
-    form.addEventListener("submit",function (e) {
+    getToken(){
+        document.getElementById("status").innerHTML = `<h3>Processing</h3>`
+        const form = document.getElementById("form")
+        form.addEventListener("submit",function (e) {
         e.preventDefault()
-
-        // const email = document.getElementById("email").value
-        // const password = document.getElementById("password").value
-
-        // fetch("http://localhost:3000/api/users/login", {
-        //     method: 'POST',
-        //     body: JSON.stringify({ email: email, password: password }),
-        //     headers: {
-        //         "Content-Type": 'application/json'
-        //     }
-        // }).then(function (response) {
-        //     return response.json()
-        // }).then(function (data) {
-        //     document.getElementById("res-div").innerHTML = `
-        //     Here is the genereted JWS token:<br>
-        //     <b><textarea name="result" id="result" cols="30" rows="10">${data.accessToken}</textarea></b>`
-        // })
-
+    
+            // const email = document.getElementById("email").value
+            // const password = document.getElementById("password").value
+    
+            // fetch("http://localhost:3000/api/users/login", {
+            //     method: 'POST',
+            //     body: JSON.stringify({ email: email, password: password }),
+            //     headers: {
+            //         "Content-Type": 'application/json'
+            //     }
+            // }).then(function (response) {
+            //     return response.json()
+            // }).then(function (data) {
+            //     document.getElementById("res-div").innerHTML = `
+            //     Here is the genereted JWS token:<br>
+            //     <b><textarea name="result" id="result" cols="30" rows="10">${data.accessToken}</textarea></b>`
+            // })
+    
         const formdata = new FormData(form);
         const data = Object.fromEntries(formdata);
         // console.log(data);
@@ -55,64 +32,32 @@ function getToken() {
             method:"POST",
             headers:{ 'Content-Type':'application/json'},
             body: JSON.stringify(data)
-        }).then(res => res.json())
-          .then(data=> {
-          document.getElementById("res-div").innerHTML = `
-          Here is the genereted JWS token:<br>
-          <b><textarea name="result" id="result" cols="30" rows="10">${data.accessToken}</textarea></b>`
-          }
-          )
-          .catch(err=>console.log(err))
-
-    })
-}
-
-async function allusers() {
-    let result = await fetch("http://localhost:3000/api/users/all");
-    result = await result.json();
-    // console.log(result);
-    document.getElementById("tablehead").innerHTML = `
-    <tr>
-        <td>Id </td>
-        <th>Username </th>
-        <th>Email </th>
-        <th>Password</th>
-    </tr>`
-    document.getElementById("userData").innerHTML = result
-        .map((user) =>
-            `<tr>
-        <td>${user._id}</td>
-        <td>${user.username}</td>
-        <td>${user.email}</td>
-        <td>${user.password}</td>
-    </tr>`
-        ).join("");
-}
-
-function tokenWindow() {
-    document.getElementById("login-window").innerHTML = `
-    <div>
-        <form id="form" method="post">
-        <!-- <form action="/api/users/login" method="post"> -->
-            <input type="text" name="email" id="email" placeholder="Enter the Email"><br>
-            <input type="text" name="password" id="password" placeholder="Enter the password"><br>
-            <button type="submit" onclick="getToken()">Get Token</button>
-        </form>
-`}
-
-async function sendtoken(){
-    const token = document.getElementById("token-field").values
-    // const result = await fetch("http://localhost:3000/api/contacts",{})
-    reset();
-    document.getElementById("sendingtokenwindow").innerHTML = `
-        <button type="button" class="btn-login" onclick="read()">Read</button>
-        <button type="button" class="btn-login" onclick="()">Add</button>
-        <button type="button" class="btn-login" onclick="()">Update</button>
-        <button type="button" class="btn-login" onclick="()">Delete</button>`
-}
-
-async function read(){
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoicGVyc29uMiIsImVtYWlsIjoicGVyc29uMkBnbWFpbC5jb20iLCJpZCI6IjY0YTRlNzg1MjAwZDRjMDU3YmJmZjFmZSJ9LCJpYXQiOjE2ODg3NDA0NTAsImV4cCI6MTY4ODc0MTM1MH0.5TTrORkDty4C2aXO4m1V1kyY_dFAlg3U0icSfPz_S70"
+        })
+        .then(res => res.json())
+        .then(data=> {
+        //   document.getElementById("res-div").innerHTML = `
+        //   Here is the genereted JWS token:<br>
+        //   <b><textarea name="result" id="result" cols="30" rows="10">${data.accessToken}</textarea></b>`
+            if(!data.accessToken){
+                document.getElementById("status").innerHTML = `<h3>Unauthorized User</h3>`
+                console.warn("Unauthorized User")
+                return
+            }
+            token = data.accessToken
+            // document.getElementById("tr").innerHTML = `${this.token}`
+            document.getElementById("status").innerHTML = `<h3>Completed</h3>`
+            console.log("token is set.")
+            loggedWindow();
+        })
+        .catch(err=>{
+            console.log(err)})
+            document.getElementById("status").innerHTML = `<h3>Failed</h3>`
+        })
+    }
+    create(){
+        console.log("creating...")
+    }
+    async read(){
     let result = await fetch("http://localhost:3000/api/contacts",{
             method:"GET",
             headers:{
@@ -134,5 +79,118 @@ async function read(){
         <td>${user.phone}</td>
     </tr>`
         ).join("");
+        
+        // console.log(token)
+    }
+    update(){
+        console.log("updating...")
+    }
+    delete(){
+        console.log("deleting...")
+    }
+}
+
+function reset() {
+    document.getElementById("userWindow").innerHTML = "";
+    document.getElementById("res-div").innerHTML = "";
+    document.getElementById("tablehead").innerHTML = "";
+    document.getElementById("userData").innerHTML = "";
+}
+
+async function allusers() {
+    try {
+    document.getElementById("status").innerHTML = `<h3>Processing</h3>`
+    let result = await fetch("http://localhost:3000/api/users/all")
+    result = await result.json();
+    // console.log(result);
+    document.getElementById("tablehead").innerHTML = `
+    <tr>
+        <td>Id </td>
+        <th>Username </th>
+        <th>Email </th>
+        <th>Password</th>
+    </tr>`
+    document.getElementById("userData").innerHTML = result
+        .map((user) =>
+            `<tr>
+        <td>${user._id}</td>
+        <td>${user.username}</td>
+        <td>${user.email}</td>
+        <td>${user.password}</td>
+    </tr>`
+        ).join("");
+    document.getElementById("status").innerHTML = `<h3>Completed</h3>`
+    } catch (error) {
+        document.getElementById("status").innerHTML = `<h3>Failed</h3>`
+        console.log(error)
+    }
     
+}
+
+function loginWindow() {
+    document.getElementById("userWindow").innerHTML = `
+    <div>
+        <form id="form" method="post">
+        <!-- <form action="/api/users/login" method="post"> -->
+        <input type="text" name="email" id="email" placeholder="Enter the Email"><br>
+        <input type="text" name="password" id="password" placeholder="Enter the password"><br>
+        <button type="submit" onclick="login()">Get Token</button>
+        </form>
+    </div>
+`}
+
+function loggedWindow(token){
+    // const token = document.getElementById("token-field").values
+    // const result = await fetch("http://localhost:3000/api/contacts",{})
+    reset();
+    document.getElementById("userWindow").innerHTML = `
+        <button type="button" class="btn-login" onclick="read()">Read</button>
+        <button type="button" class="btn-login" onclick="add()">Add</button>
+        <button type="button" class="btn-login" onclick="updateData()">Update</button>
+        <button type="button" class="btn-login" onclick="deleteData()">Delete</button>`
+}
+function getTokenOuter(){
+    document.getElementById("status").innerHTML = `<h3>Processing</h3>`
+        const form = document.getElementById("form")
+        form.addEventListener("submit",function (e) {
+        e.preventDefault()
+    
+            const email = document.getElementById("email").value
+            const password = document.getElementById("password").value
+    
+            fetch("http://localhost:3000/api/users/login", {
+                method: 'POST',
+                body: JSON.stringify({ email: email, password: password }),
+                headers: {
+                    "Content-Type": 'application/json'
+                }
+            }).then(function (response) {
+                return response.json()
+            }).then(function (data) {
+                document.getElementById("res-div").innerHTML = `
+                Here is the genereted JWS token:<br>
+                <b><textarea name="result" id="result" cols="30" rows="10">${data.accessToken}</textarea></b>`
+            })
+})
+}
+
+
+const user = new User()
+
+function login(){
+    user.getToken()
+}
+
+function add(){
+    user.create()
+}
+function read(){
+    user.read()
+    
+}
+function updateData(){
+    user.update()
+}
+function deleteData(){
+    user.delete()
 }
