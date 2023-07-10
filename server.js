ee5012fa24b3dd71689320172b914b976d1937e7
Lokeswar Gahir <lokeswargahir349@gmail.com>
2023-07-10 12:26:07 +0530

    changed port from 5000 to 3000(not so important). hosted on server. workin fine

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDb();
 const cors = require("cors");
 app.use(cors());
 
-const port  = process.env.PORT || 5000;
+const port  = process.env.PORT || 3000;
 // const port  = 3001;
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
